f3265115327a29d2f63a9cca8916b6ccce249a74
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-09-06 08:21:49 +0700

    Send Correlation ID #nv3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main {
        serverSocket.setReuseAddress(true);
        // Wait for connection from client.
        clientSocket = serverSocket.accept();
+       clientSocket.sendUrgentData(7);
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {
