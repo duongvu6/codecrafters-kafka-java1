89d009d3e28451c4815b1b34a6e16018c21869ac
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-09-06 08:26:00 +0700

    Send Correlation ID #nv3 fix 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,8 +20,7 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("7\r\n".getBytes());
+            clientSocket.getOutputStream().write(new byte[] {0, 0, 0, 0, 0, 0, 0, 7});
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
